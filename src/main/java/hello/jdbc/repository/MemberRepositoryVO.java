5f375ee14aeae7fd722f1eb522374107d9e49c88
jmpark <jmpark@interezen.co.kr>
2023-06-25 20:18:18 +0900

    feat: jdbc 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryVO.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryVO.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -17,7 +18,7 @@ public class MemberRepositoryVO {
 
         Connection con = null; // 커넥션이 있어야 연결 가능
         PreparedStatement pstmt = null; // 이걸로 database에 쿼리를 날림
-        /*바인딩 기능이 추가된 것일 뿐 그것을 제외하고 statement클래스와는 기능이 동일 상속받은 것이다.*/
+        /* 바인딩 기능이 추가된 것일 뿐 그것을 제외하고 statement클래스와는 기능이 동일 상속받은 것이다. */
         try {
             con = getConnection();
             pstmt = con.prepareStatement(sql);
@@ -30,16 +31,48 @@ public class MemberRepositoryVO {
             throw e; // 로그정도 남기고 exception 다시 밖으로 던지기 (throws SQLException)
         } finally {
             /*
-            * 시작과 역순으로 close처리
-            * 항상 보장되도록 finally로
-            */
+             * 시작과 역순으로 close처리
+             * 항상 보장되도록 finally로
+             */
             close(con, pstmt, null);
         }
 
     }
 
+    /** 회원 조회 추가 */
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql); // connection을 통해서 prepareStatement을 얻어와야한다.
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                /* 데이터가 존재하지 않음
+                 * NoSuchElementException java 기본 예외
+                 * memberId 넣어주는게 좋음. 문제가 생겼을때 못 찾을 수 있다. 이런 키값을 넣어주는게 좋음
+                 * */
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            /* 리소스 닫아줘야한다. */
+            close(con, pstmt, rs);
+        }
+    }
+
     /**
-     *
+     * 리소스 닫아주는 method
      */
     private void close(Connection con, Statement stmt, ResultSet rs) {
         if (rs != null) {
--- src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -1,21 +1,29 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 
+
+@Slf4j
 class MemberRepositoryVOTest {
 
     MemberRepositoryVO repository = new MemberRepositoryVO();
 
     @Test
     void crud() throws SQLException {
-        //save
+        // save
         Member member = new Member("memberV1", 10000);
         repository.save(member);
-    }
 
+        // findByID
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+    }
 
 }
