708a7a0703233cd2a9a7e53c453a6d4b1e3c8dfe
myungyi <wlsaud1992@naver.com>
2023-06-26 06:02:22 +0900

    feat: jdbc 조회

--- src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -17,12 +17,16 @@ class MemberRepositoryVOTest {
     @Test
     void crud() throws SQLException {
         // save
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
 
         // findByID
         Member findMember = repository.findById(member.getMemberId());
-        log.info("findMember={}", findMember);
+        log.info("findMember = {}", findMember);
+        log.info("member == findMember {}", member == findMember);
+        // equals hashcode를 자동으로 만들어줌. 모든 필드를 가지고 equals를 만들어줌
+        // log.info("member equals findMember {}", member.equals(findMember));
+        // 우리가 찾은 맴버는 == member 객체와 같다.
         assertThat(findMember).isEqualTo(member);
     }
 
